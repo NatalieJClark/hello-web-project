61a5ed8c4ef499e557cf3afe4ba9a61fa9285ef3
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-11 16:03:27 +0100

    Add integration test for names route

--- tests/test_app.py
+++ tests/test_app.py
@@ -84,6 +84,37 @@ def test_post_sort_names_with_no_list_of_names(web_client):
     assert response.status_code == 400
     assert response.data.decode('utf-8') == "You didn't submit any names!"
 
+"""
+When: we request GET /names
+With: a query parameter add=Eddie
+Then: we should get a 200 response and "Alice, Eddie, Julia, Karim"
+"""
+def test_get_names_with_one_name(web_client):
+    response = web_client.get('/names?add=Eddie')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Alice, Eddie, Julia, Karim"
+
+"""
+When: we request GET /names
+With: mulitple names add=Eddie,Leo
+Then: we should get a 200 response and "Alice, Eddie, Julia, Karim, Leo"
+"""
+def test_get_names_with_multiple_names(web_client):
+    response = web_client.get('/names?add=Eddie,Leo')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Alice, Eddie, Julia, Karim, Leo"
+
+"""
+When: we request GET /names
+With: no query parameters
+Then: we should get a 400 response and "You didn't submit names to add to the list 'Alice, Julia, Karim'"
+"""
+def test_get_names_with_no_parameters(web_client):
+    response = web_client.get('/names')
+    assert response.status_code == 400
+    assert response.data.decode('utf-8') == "You didn't submit names to add to the list 'Alice, Julia, Karim'"
+
+
 # === Example Code Below ===
 
 """
